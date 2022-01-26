9ee379ece0f3b06678dc9d4a5f6f078fdbb7933d
RandomPug <qbz55688@gmail.com>
2022-01-26 12:01:15 +0800

    第一版網頁 RandomMission ver.1

---
+++ react/src/page/map-pack.js
@@ -0,0 +1,38 @@
+export const MAP_NAME = {
+    1: '213 Park Homes(貧民窟)',
+    2: '4U Gas Station(加油站/小餐廳)',
+    3: 'Wenderly Hills Hotel(大旅館)',
+    4: 'Hotel Rooms(長長陽台的測試關1)',
+    5: 'Penthouse(找槍的測試關2)',
+    6: 'Caesars Cars Dealership(車店)',
+    7: 'Port Hoken(假港口真妓院)',
+    8: 'Cherryessa Farm(傭兵莊園)',
+}
+
+
+export const MAP_PIC = {
+    1: '213_park_homes.png',
+    2: '4u_gas_station.png',
+    3: 'wenderly_hills_hotel.png',
+    4: 'hotel_room.png',
+    5: 'penthouse.png',
+    6: 'caesars_cars_dealershop.png',
+    7: 'port_hoken.png',
+    8: 'cherryssa_farm.png'
+}
+
+export const MAP_MODE = {
+    1: 'Barricaded Suspect',
+    2: 'RAID',
+    3: 'Active Shooter',
+    4: 'Bomb Threat',
+    5: 'Hostage Rescue'
+}
+
+export const MODE_PIC = {
+    1: 'barricaded_suspects.png',
+    2: 'raid.png',
+    3: 'active_shooter.png',
+    4: 'bomb_threat.png',
+    5: 'hostage_rescue.png'
+}
